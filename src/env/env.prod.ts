52594ad118c6d2e9848530aad01e50c7f5e8eb20
Luc Joosten <lhajoosten@outlook.com>
2025-04-14 00:20:01 +0200

    updated env value

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     github: {
         clientId: 'Ov23li4pCX49borQ7Onb',
-        redirectUri: 'https://lhajoosten.github.io/CoberturaViewer/auth/callback',
+        redirectUri: 'https://cobertura-viewer.vercel.app/auth/callback',
         scope: 'read:user user:email'
     },
     apiBaseUrl: 'https://o-auth-github-cobertura-ap3pev4ib-lucs-projects-55fbeb36.vercel.app/api'
